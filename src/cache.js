786b4f6a2857eab2cd35a0ec3c53afa0080e44e9
booninja <vav18@hi.is>
2021-03-10 23:53:59 +0000

    comment cache out

--- src/cache.js
+++ src/cache.js
@@ -1,29 +1,29 @@
-// TODO útfæra redis cache
-import redis from 'redis';
-import { promisify } from 'util';
+// // TODO útfæra redis cache
+// import redis from 'redis';
+// import { promisify } from 'util';
 
-const client = redis.createClient({
-  url: 'redis://127.0.0.1:6379/0',
-});
+// const client = redis.createClient({
+//   url: 'redis://127.0.0.1:6379/0',
+// });
 
-const asyncGet = promisify(client.get).bind(client);
-const asyncSet = promisify(client.set).bind(client);
+// const asyncGet = promisify(client.get).bind(client);
+// const asyncSet = promisify(client.set).bind(client);
 
-/**
- * Get earthquakes from the cache
- * @param {*} key
- * @returns
- */
-export async function getEarthquakes(key) {
-  const earthquakes = await asyncGet(key);
-  return earthquakes;
-}
+// /**
+//  * Get earthquakes from the cache
+//  * @param {*} key
+//  * @returns
+//  */
+// export async function getEarthquakes(key) {
+//   const earthquakes = await asyncGet(key);
+//   return earthquakes;
+// }
 
-/**
- * Add earthquakes to cache
- * @param {*} key
- * @param {*} earthquakes
- */
-export async function setEarthquakes(key, earthquakes) {
-  await asyncSet(key, earthquakes);
-}
+// /**
+//  * Add earthquakes to cache
+//  * @param {*} key
+//  * @param {*} earthquakes
+//  */
+// export async function setEarthquakes(key, earthquakes) {
+//   await asyncSet(key, earthquakes);
+// }
