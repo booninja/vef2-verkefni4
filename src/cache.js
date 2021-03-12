e9f121210026035e0d39be6ed65376f477310e8c
booninja <vav18@hi.is>
2021-03-12 17:50:42 +0000

    fix dotenv

--- src/cache.js
+++ src/cache.js
@@ -1,17 +1,23 @@
 import redis from 'redis';
 import { promisify } from 'util';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export let getEarthquakes; // eslint-disable-line import/no-mutable-exports
 export let setEarthquakes; // eslint-disable-line import/no-mutable-exports
 
-const client = redis.createClient({
-  url: 'redis://127.0.0.1:6379/0',
-});
+const {
+  REDIS_URL: redisURL,
+} = process.env;
 
 try {
-  if(!client) {
+  if (!redisURL) {
     throw Error('Redis url not found');
   }
+  const client = redis.createClient({
+    url: redisURL,
+  });
 
   const asyncGet = promisify(client.get).bind(client);
   const asyncSet = promisify(client.set).bind(client);
@@ -24,7 +30,7 @@ try {
   getEarthquakes = async (key) => {
     const earthquakes = await asyncGet(key);
     return earthquakes;
-  }
+  };
 
   /**
    * Add earthquakes to cache
@@ -33,9 +39,9 @@ try {
    */
   setEarthquakes = async (key, earthquakes) => {
     await asyncSet(key, earthquakes);
-  }
+  };
 } catch (e) {
-  console.error('Error setting up redis client, running without cache', e);
-  getEarthquakes = async (key) => false; // eslint-disable-line no-unused-vars  
+  console.error('Error setting up redis redisURL, running without cache', e);
+  getEarthquakes = async (key) => false; // eslint-disable-line no-unused-vars
   setEarthquakes = async (key, value) => { }; // eslint-disable-line no-unused-vars
 }
