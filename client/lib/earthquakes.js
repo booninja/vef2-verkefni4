fed8aba0f3a6cc0c3bb0cff6af2c3e16b9d93f67
booninja <vav18@hi.is>
2021-03-12 16:21:36 +0000

    trying to fix merge conflicts

--- client/lib/earthquakes.js
+++ client/lib/earthquakes.js
@@ -2,12 +2,10 @@
 export async function fetchEarthquakes(type, period) {
   // TODO sækja gögn frá proxy þjónustu
   let result;
-  const URL = `https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/${period}_${type}.geojson`;
+  // const URL = `https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/${period}_${type}.geojson`;
 
   try {
-    // result = await fetch(URL);
-    // result = await fetch(`http://localhost:3001/proxy?period=${period}&type=${type}`);
-    result = await fetch(URL);
+    result = await fetch(`http://localhost:3001/proxy?period=${period}&type=${type}`);
   } catch (e) {
     console.error('Villa við að sækja', e);
     return null;
